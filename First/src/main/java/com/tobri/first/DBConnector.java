2a67b07c57f84c91a626b6698cd5cc94e0b7c4fc
studat <sxxxxx@htw-dresden.de>
2013-11-28 13:56:57 +0100

    DBConnector Rumpf implementiert

--- First/src/main/java/com/tobri/first/DBConnector.java
+++ First/src/main/java/com/tobri/first/DBConnector.java
@@ -19,7 +19,7 @@ public class DBConnector {
         this.localHistory = SQLiteDatabase.openDatabase(this.dbName, null, SQLiteDatabase.CREATE_IF_NECESSARY);
     }
 
-    public boolean UserDefined(String username) {
+    public boolean UserDefined() {
         if (null != this.localHistory) {
             Cursor c = this.localHistory.query(this.table, new String[]{"Login"}, null, null, null, null, null);
             if (c.getCount() > 0) {
